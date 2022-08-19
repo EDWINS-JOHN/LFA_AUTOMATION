a3622979285eba11c9a575d218611dbc33434936
Mamba-Cita <edwnsjohn52@gmail.cm>
2022-08-19 16:02:25 +0300

    logs added

--- src/testCases/test_login.py
+++ src/testCases/test_login.py
@@ -1,20 +1,28 @@
 import pytest
 from selenium import webdriver
 from pageObjects.LoginPage import LoginPage
+from utilities.readproperties import ReadConfig
+from utilities.customLogger import  LogGen
 
 
 class Test_01_Login:
-    baseURL="https://tsavo.welovetrucks.co/login/"
-    phone = "716258644"
-    password ="gigi-Mamba2006"
+    baseURL = ReadConfig.getAppUrl()
+    phone = ReadConfig.getPhone()
+    password = ReadConfig.getPassword()
+    
+    logger = LogGen.loggen()
 
     def test_homePageTitle(self,setup):
+        
+        self.logger.info("*******************Test_01_Login************")
+        self.logger.info("*******************Checking  Homepage Title************")
         self.driver=setup
         self.driver.get(self.baseURL)
         actual_title=self.driver.title
         if actual_title == "Lori Field App":
             assert True
             self.driver.close()
+            self.logger.info("*******************Test passed************")
         else:
             self.driver.save_screenshot("..\\ScreenShots\\"+"test_homePagetitle.png")
             self.driver.close()
@@ -32,6 +40,7 @@ class Test_01_Login:
         if page_title == "Lori Field App":
             assert True
             self.driver.close()
+            self.logger.info("*******************Login Test Passed************")
         else:
             self.driver.save_screenshot("..\\ScreenShots\\"+"test_login.png")
             self.driver.close()
